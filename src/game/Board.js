e89bcc6bc5ed3a6949f5a02e073306dc13bcaf1d
fabrids23 <fabrids23@gmail.com>
2020-05-27 02:24:28 -0300

    when clicking to rotate a piece, it moves all the other pieces

--- src/game/Board.js
+++ src/game/Board.js
@@ -6,12 +6,124 @@ import {Piece} from "./Piece";
 export class Board extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
+
         // Initialize component state
         this.state = {
             grid: Array(100).fill(false),
+            pieces: [
+                {
+                    key:0,
+                    defaultY: 0,
+                    defaultX: 50,
+                    pos:[],
+                    isRotated: false,
+                    length: 4,
+                },
+                {
+                    key:1,
+                    defaultY: 50,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 3,
+
+                },
+                {
+                    key:2,
+                    defaultY: 100,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 3,
+                },
+                {
+                    key:3,
+                    defaultY: 150,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 2,
+                },
+                {
+                    key:4,
+                    defaultY: 200,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 2,
+                },
+                {
+                    key:5,
+                    defaultY: 250,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 2,
+                },
+                {
+                    key:6,
+                    defaultY: 300,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 1,
+                },
+                {
+                    key:7,
+                    defaultY: 350,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 1,
+                },
+                {
+                    key:8,
+                    defaultY: 400,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 1,
+                },
+                {
+                    key:9,
+                    defaultY: 450,
+                    defaultX: 150,
+                    pos:[],
+                    isRotated: false,
+                    length: 1,
+                },
+            ]
         }
     }
+
+    handleRotate(key){
+        this.setState({
+            ...this.state,
+            pieces: [
+                ...this.state.pieces.slice(0,key),
+                {
+                    ...this.state.pieces[key],
+                    isRotated: !this.state.pieces[key].isRotated,
+                },
+                ...this.state.pieces.slice(key+1)
+            ]
+        });
+    }
+
+    handleStart(key){
+
+    }
+
+    handleStop(key){
+
+    }
+
+    handleDrag(key){
+
+    }
+
+
 
     render() {
 
@@ -140,26 +252,98 @@ export class Board extends Component {
                         <Box value={this.state.grid[i++]}/>
                     </div>
                 </div>
-
-                <div className="board" style={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    paddingTop: '10%',
-                    paddingRight: '5%',
-                    flexWrap: 'wrap',
-                }}>
-                    <Piece length={4} isRotated={false}/>
-                    <Piece length={3} isRotated={false}/>
-                    <Piece length={3} isRotated={false}/>
-                    <Piece length={2} isRotated={false}/>
-                    <Piece length={2} isRotated={false}/>
-                    <Piece length={2} isRotated={false}/>
-                    <Piece length={1} isRotated={false}/>
-                    <Piece length={1} isRotated={false}/>
-                    <Piece length={1} isRotated={false}/>
-                    <Piece length={1} isRotated={false}/>
+                <div className="board">
+                    <Piece length={this.state.pieces[0].length}
+                           isRotated={this.state.pieces[0].isRotated}
+                           originalX={this.state.pieces[0].defaultX} originalY={this.state.pieces[0].defaultY}
+                           bounds={{top: 0, left: -650, right: 0, bottom: 450}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[0].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[0].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[0].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[0].key)}
+                    />
+                    <Piece length={this.state.pieces[1].length}
+                           isRotated={this.state.pieces[1].isRotated}
+                           originalX={this.state.pieces[1].defaultX} originalY={this.state.pieces[1].defaultY}
+                           bounds={{top: -50, left: -650, right: 0, bottom: 400}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[1].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[1].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[1].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[1].key)}
+                    />
+                    <Piece length={this.state.pieces[2].length}
+                           isRotated={this.state.pieces[2].isRotated}
+                           originalX={this.state.pieces[2].defaultX} originalY={this.state.pieces[2].defaultY}
+                           bounds={{top: -100, left: -650, right: 0, bottom: 350}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[2].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[2].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[2].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[2].key)}
+                    />
+                    <Piece length={this.state.pieces[3].length}
+                           isRotated={this.state.pieces[3].isRotated}
+                           originalX={this.state.pieces[3].defaultX} originalY={this.state.pieces[3].defaultY}
+                           bounds={{top: -150, left: -650, right: 0, bottom: 300}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[3].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[3].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[3].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[3].key)}
+                    />
+                    <Piece length={this.state.pieces[4].length}
+                           isRotated={this.state.pieces[4].isRotated}
+                           originalX={this.state.pieces[4].defaultX} originalY={this.state.pieces[4].defaultY}
+                           bounds={{top: -200, left: -650, right: 0, bottom: 250}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[4].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[4].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[4].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[4].key)}
+                    />
+                    <Piece length={this.state.pieces[5].length}
+                           isRotated={this.state.pieces[5].isRotated}
+                           originalX={this.state.pieces[5].defaultX} originalY={this.state.pieces[5].defaultY}
+                           bounds={{top: -250, left: -650, right: 0, bottom: 200}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[5].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[5].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[5].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[5].key)}
+                    />
+                    <Piece length={this.state.pieces[6].length}
+                           isRotated={this.state.pieces[6].isRotated}
+                           originalX={this.state.pieces[6].defaultX} originalY={this.state.pieces[6].defaultY}
+                           bounds={{top: -300, left: -650, right: 0, bottom: 150}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[6].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[6].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[6].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[6].key)}
+                    />
+                    <Piece length={this.state.pieces[7].length}
+                           isRotated={this.state.pieces[7].isRotated}
+                           originalX={this.state.pieces[7].defaultX} originalY={this.state.pieces[7].defaultY}
+                           bounds={{top: -350, left: -650, right: 0, bottom: 100}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[7].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[7].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[7].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[7].key)}
+                    />
+                    <Piece length={this.state.pieces[8].length}
+                           isRotated={this.state.pieces[8].isRotated}
+                           originalX={this.state.pieces[8].defaultX} originalY={this.state.pieces[8].defaultY}
+                           bounds={{top: -400, left: -650, right: 0, bottom: 50}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[8].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[8].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[8].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[8].key)}
+                    />
+                    <Piece length={this.state.pieces[9].length}
+                           isRotated={this.state.pieces[9].isRotated}
+                           originalX={this.state.pieces[9].defaultX} originalY={this.state.pieces[9].defaultY}
+                           bounds={{top: -450, left: -650, right: 0, bottom: 0}}
+                           handleStart={this.handleStart.bind(this, this.state.pieces[9].key)}
+                           handleStop={this.handleStop.bind(this, this.state.pieces[9].key)}
+                           handleDrag={this.handleDrag.bind(this, this.state.pieces[9].key)}
+                           rotate={this.handleRotate.bind(this, this.state.pieces[9].key)}
+                    />
                 </div>
-
-                </div>)
+            </div>)
     }
 }
--- src/game/Piece.js
+++ src/game/Piece.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './GameStyleSheet.css'
-
+import Draggable from 'react-draggable'
 
 export const Piece = (props) => {
 
@@ -9,7 +9,7 @@ export const Piece = (props) => {
     let style = {
         width: widthValue + 'px',
         height: '50px',
-    }
+    };
 
     if (props.isRotated) {
         const oldWidth = style.width;
@@ -18,10 +18,25 @@ export const Piece = (props) => {
     }
 
     return(
-        <div className='piece' style={style}>
-            {console.log("WIDTH " + style.width)}
-            {console.log("HEIGHT " + style.height)}
-            {console.log("-------------------")}
-        </div>
+        <Draggable
+            axis="both"
+            bounds={props.bounds}
+            handle=".handle"
+            defaultPosition={{x: 0, y: 0}}
+            grid={[50, 50]}
+            scale={1}
+            onStart={props.handleStart}
+            onStop={props.handleStop}
+            onDrag={props.handleDrag}
+
+        >
+            <div className={"handle"}>
+                <div className='piece' style={style} onClick={props.rotate}>
+                    {console.log("WIDTH " + style.width)}
+                    {console.log("HEIGHT " + style.height)}
+                    {console.log("-------------------")}
+                </div>
+            </div>
+        </Draggable>
     )
 };
