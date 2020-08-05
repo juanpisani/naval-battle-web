210ae64744ea256e2c56895d272ab481d2af9480
fabrids23 <fabrids23@gmail.com>
2020-08-04 21:40:58 -0300

    New path

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 // const API_PATH = 'ws://localhost:5000';
-const API_PATH = 'ws://ec2-54-237-31-62.compute-1.amazonaws.com:5000';
+// const API_PATH = 'ws://ec2-54-237-31-62.compute-1.amazonaws.com:5000';
+const API_PATH = "ws://ec2-100-26-230-254.compute-1.amazonaws.com:5000";
+
 
 export default {
     API_PATH
--- src/session/session.services.js
+++ src/session/session.services.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const baseUrl = "http://localhost:5000/api/";
-const baseUrl = "http://ec2-54-237-31-62.compute-1.amazonaws.com:5000/api/";
+// const baseUrl = "http://ec2-54-237-31-62.compute-1.amazonaws.com:5000/api/";
+const baseUrl = "http://ec2-100-26-230-254.compute-1.amazonaws.com:5000/api/";
 
 export const services = {
     backEndLogin: (token) =>  axios.post(baseUrl + 'auth', {id_token: token}),
